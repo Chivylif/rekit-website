84e9e6658aa82e1f6e9b6e22ac50ff21b7f3f745
AJ <ajiriosiobe@gmail.com>
2026-05-13 14:48:42 +0100

    added applink

--- components/SVG/svgIconPack.jsx
+++ components/SVG/svgIconPack.jsx
@@ -1,49 +1,15 @@
-import star from "../../assets/svg/star.svg";
-// import banner1 from "@/assets/svg/banner1.svg";
-// import banner2 from "@/assets/svg/banner2.svg";
-// import banner3 from "@/assets/svg/banner3.svg";
-// import stackedCoins from "@/assets/svg/stackedCoins.svg";
-import phoneMockup from "../../assets/svg/phoneMockup.svg";
-// import makeAPhoneCall from "../../assets/svg/makeAPhoneCall.svg";
 import appleStoreBadge from "../../assets/svg/appleStoreBadge.svg";
 import googleStoreBadge from "../../assets/svg/googleStoreBadge.svg";
 import playStoreBadgeMini from "../../assets/svg/playStoreBadgeMini.svg";
 import appleStoreBadgeMini from "../../assets/svg/appleStoreBadgeMini.svg";
-import authorImage from "../../assets/svg/authorImage.svg";
-import call from "../../assets/svg/call.svg";
-import email from "../../assets/svg/email.svg";
-import location from "../../assets/svg/location.svg";
-import facebook from "../../assets/svg/facebook.svg";
-import x from "../../assets/svg/x.svg";
-import instagram from "../../assets/svg/instagram.svg";
-// import iphone from "../../assets/svg/iphone.svg";
-// import iphoneLeft from "../../assets/svg/iphone-left.svg";
-import divdot from "../../assets/svg/div-list-dot.svg";
-import key from "../../assets/svg/key.svg"
+
 
 export const svgIconPack = {
-  star,
-  // banner1,
-  // banner2,
-  // banner3,
-  // stackedCoins,
-  phoneMockup,
-  // makeAPhoneCall,
   appleStoreBadge,
   googleStoreBadge,
   playStoreBadgeMini,
   appleStoreBadgeMini,
-  authorImage,
-  call,
-  email,
-  location,
-  // iphone,
-  facebook,
-  x,
-  instagram,
-  // iphoneLeft,
-  divdot,
-  key
+
 };
 
 // export type SvgIconPackType = keyof typeof svgIconPack;
